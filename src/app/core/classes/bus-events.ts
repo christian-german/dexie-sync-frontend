7291465f9d06523bab1ab786cb6561a2b4af62c8
TREHOUT Nathan <nathan.trehout-ext@evolution-xy.fr>
2021-07-09 18:42:29 +0200

    feat: WIP Utilisation basique d'un case d'event pour obtenir un payload typé

--- src/app/core/classes/bus-events.ts
+++ src/app/core/classes/bus-events.ts
@@ -1,22 +1,35 @@
 import { IEvent } from '../interfaces/IEvent';
-import { StoreIdentity } from '../interfaces/store-identity';
 
 export class StoreEvents implements IEvent {
-  public static STORE_CACHE_ADD = 'STORE_CACHE_ADD';
+  public static STORE_CACHE_ADDED = 'STORE_CACHE_ADD';
   public static STORE_CACHE_REMOVE = 'STORE_CACHE_REMOVE';
   public static STORE_REGISTER = 'STORE_REGISTER';
 }
 
 export interface Event {
-  payload: any;
+  payload?: any;
 }
 
-export interface StoreCacheAddEvent extends Event {
+export interface StoreCacheAddedEvent<T> extends Event {
+  tableName: string;
+  payload: {
+    addedItems: T[]
+  }
+}
+
+export interface StoreRegisterEvent extends Event {
   tableName: string;
 }
 
+export interface CurrentRevisionChangeEvent extends Event {
+  payload: {
+    currentRevision: number;
+  }
+}
+
 export class DexieEvents implements IEvent {
-  public static DEXIE_START_SYNC: string = "DEXIE_START_SYNC";
-  public static DEXIE_END_SYNC: string = "DEXIE_END_SYNC";
-  public static DEXIE_TABLE_CHANGE: string = "DEXIE_TABLE_CHANGE";
+  public static DEXIE_START_SYNC: string = 'DEXIE_START_SYNC';
+  public static DEXIE_CURRENT_REVISION_CHANGE: string = 'DEXIE_CURRENT_REVISION_CHANGE';
+  public static DEXIE_END_SYNC: string = 'DEXIE_END_SYNC';
+  public static DEXIE_TABLE_CHANGE: string = 'DEXIE_TABLE_CHANGE';
 }
