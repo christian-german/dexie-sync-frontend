266ea49be9eea56604db7009986abdaa3d21511b
TREHOUT Nathan <nathan.trehout-ext@evolution-xy.fr>
2021-07-15 14:28:33 +0200

    feat: ajout d'un séparateur des logs toutes les 1 seconde

--- src/app/core/classes/logger.ts
+++ src/app/core/classes/logger.ts
@@ -38,5 +38,16 @@ function getAffectedColor(from: string): string {
   }
 }
 
-export const log = (from: string, message: string) => environment.production ? () => {} : console.info(`%c[${from}]: %c%s`, `color: ${getAffectedColor(from)};`, `color: black`, message)
+export let lastLog: Date = new Date();
+
+export const log = (from: string, message: string) => {
+  if (environment.production) {
+    return;
+  }
+  if (new Date().getTime() - lastLog.getTime() > 1000) {
+    console.info('------------------------------');
+    lastLog = new Date();
+  }
+  console.info(`%c[${from}]: %c%s`, `color: ${getAffectedColor(from)};`, `color: black`, message)
+};
 export const logError = (from: string, message: string) => environment.production ? () => {} : console.info(`%c[${from}]: %c%s`, `color: ${getAffectedColor(from)};`, `color: red`, message)
